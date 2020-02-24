0f1647aa56bccaccce078571d2ff3687c82c145d
EvaSingh <eva.singh@hotmail.com>
2020-02-24 13:34:35 +0100

    Add styles

---
+++ JS/contact.js
@@ -0,0 +1,10 @@
+const contactForm = document.querySelector(#contactForm);
+const nameField = document.querySelector(#nameField);
+const emailField = document.querySelector(#emailField);
+const messageField = document.querySelector(#messageField);
+const submitButton = document.querySelector(#submitButton);
+
+
+contactForm.addEventListener("keyup", function (event) {
+  
+})
