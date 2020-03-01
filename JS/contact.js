574f11a40a56e98e05489fd224c349047cfebac1
EvaSingh <eva.singh@hotmail.com>
2020-03-01 22:42:35 +0100

    Add JS validation to form and update logo link

--- JS/contact.js
+++ JS/contact.js
@@ -1,19 +1,62 @@
 const contactForm = document.querySelector("#contact-form");
-const nameInput = document.querySelector("#name-input");
-const emailInput = document.querySelector("#email-input");
-const messageInput = document.querySelector("#message-input");
-const submitButton = document.querySelector("#submit-button");
+const submit = document.querySelector("#submit-button");
 
+submit.addEventListener("click", validateForm);
 
+function validateForm(event) {
+    event.preventDefault();
+    console.log("The form was submitted");
 
-emailInput.addEventListener("input", function (event) {
-    if (emailInput.validity.typeMismatch) {
-        emailInput.setCustomValidity("Hmm....");
+    const nameInput = document.querySelector("#name-input");
+    const nameErr = document.querySelector("#name-error");
+    const nameValue = nameInput.value;
+
+    if (checkInputLength(nameValue) === true) {
+        nameErr.style.display = "none";
     } else {
-        emailInput.setCustomValidity("");
+        nameErr.style.display = "block";
     }
-});
-  
+
+    const emailInput = document.querySelector("#email-input");
+    const emailErr = document.querySelector("#email-error");
+    const invalidEmailErr = document.querySelector("#inv-email-error");
+
+    const emailValue = emailInput.value;
+
+    if (checkInputLength(emailValue) === true) {
+        emailErr.style.display = "none";
+    } else {
+        emailErr.style.display = "block";
+    }
+
+    if (validateEmail(emailValue) === true) {
+        invalidEmailErr.style.display = "none";
+    } else {
+        invalidEmailErr.style.display = "block";
+    }
+}
+
+function checkInputLength(value) {
+
+    const trimmedValue = value.trim();
+
+    
+    if (trimmedValue.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(emailInput) {
+    const regEx = /\S+@\S+\.\S+/;
+    return regEx.test(emailInput);
+}
+
+
+        
+
+
     
     
    
